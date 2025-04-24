c9f927eab0f622b9094d91ff3955667736e2c4a8
Notwebmaker <143799102+Notwebmaker@users.noreply.github.com>
2025-04-24 19:56:24 +1000

    Update and rename hello.py to start here.py
    added comments and removed unnessecary things

--- hello.py
+++
@@ -1,5 +0,0 @@
-print ("Hello world")
-bob = input ('who are you?')
-print('hi'+ bob )
-if bob == ('fart'):
-    print('you peice of crap, that not your name!')
---
+++ start here.py
@@ -0,0 +1,4 @@
+print ("Hello world")#basic print message , remember to place qutations around the message, otherwise python will think its a variable
+name = input ('who are you?') # prompt message that stores answer in a variable
+print('hi'+ name ) # prints out "hi" followed by the previous input
+
